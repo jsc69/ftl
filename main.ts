6d85f385063b8d289eff71fc2938c38102458e1f
jsc69 <54580037+jsc69@users.noreply.github.com>
2020-12-21 15:32:51 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,14 @@
-let mySprite = sprites.create(img`
+let engine = sprites.create(img`
+    1 1 1 1 1 1 1 1 
+    1 6 6 6 6 6 1 1 
+    1 1 8 5 6 6 6 1 
+    1 1 8 6 5 6 6 1 
+    1 1 8 5 6 6 6 1 
+    1 1 8 6 5 6 6 1 
+    1 6 6 6 6 6 1 1 
+    1 1 1 1 1 1 1 1 
+    `, SpriteKind.Player)
+let crewMan = sprites.create(img`
     . . f f f f . . 
     . f . f . f . . 
     . f f f f f . . 
@@ -8,9 +18,10 @@ let mySprite = sprites.create(img`
     . . . f . . . . 
     . f f . f f . . 
     `, SpriteKind.Player)
-mySprite.setPosition(24, 38)
-controller.moveSprite(mySprite, 40, 40)
-mySprite.setFlag(SpriteFlag.StayInScreen, true)
+engine.setPosition(40, 56)
+crewMan.setPosition(24, 38)
+controller.moveSprite(crewMan, 40, 40)
+crewMan.setFlag(SpriteFlag.StayInScreen, true)
 tiles.setTilemap(tiles.createTilemap(hex`0a00080009090909090909090509090909090909090706080b0404040209090606060b0404040401090606060b040404030909060606090909090909090a0a0a0909090909090909090909090909090909090909`, img`
     . . . . . . . . . . 
     . . . . . . . . . . 
